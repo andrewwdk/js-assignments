70dc744f9220817ba497759be8eabbb3b0998485
andrewwdk <zhidenko_1999@mail.ru>
2018-04-16 22:31:57 +0300

    Update the 48th task

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -281,7 +281,7 @@ function getSecondItems(arr) {
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
 function propagateItemsByPositionIndex(arr) {
-   throw new Error('Not implemented');
+  return arr.reduce((answer, elem ,i ) => answer.concat(Array.from({length: i+1}, ()=>elem)),[]);
 }
 
 
