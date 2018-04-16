e6b7355bbf5f35bf77ad75a96bc82cb7e99f398e
andrewwdk <zhidenko_1999@mail.ru>
2018-04-16 19:55:40 +0300

    Update the 38th task

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -105,7 +105,7 @@ function getArrayOfStrings(arr) {
  *    [ false, 0, NaN, '', undefined ]   => [ ]
  */
 function removeFalsyValues(arr) {
-   throw new Error('Not implemented');
+   return arr.filter(value => value);
 }
 
 /**
