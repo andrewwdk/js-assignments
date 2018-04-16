b9a68dec493469c081783b363f99a48755f2ea44
andrewwdk <zhidenko_1999@mail.ru>
2018-04-16 20:40:40 +0300

    Update the 40th task

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -134,7 +134,7 @@ function getUpperCaseStrings(arr) {
  *    [ 'angular', 'react', 'ember' ] => [ 7, 5, 5 ]
  */
 function getStringsLength(arr) {
-   throw new Error('Not implemented');
+   return arr.map(function(value){ return value.length});
 }
 
 /**
