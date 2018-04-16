2db2ae1a67244679f4b64ba9b9632c77bed0a35d
andrewwdk <zhidenko_1999@mail.ru>
2018-04-16 20:50:26 +0300

    Update the 43th task

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -178,7 +178,7 @@ function getHead(arr, n) {
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'b', 'c', 'd' ]
  */
 function getTail(arr, n) {
-   throw new Error('Not implemented');
+   return arr.slice(arr.length -n, arr.length);
 }
 
 
