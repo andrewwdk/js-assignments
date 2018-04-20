3b05ef45972c4915a72e3feee09f36d9fd46e34c
andrewwdk <zhidenko_1999@mail.ru>
2018-04-20 12:11:11 +0300

    Update the 73th task

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -140,7 +140,11 @@ function isTriangle(a,b,c) {
  *
  */
 function doRectanglesOverlap(rect1, rect2) {
-    throw new Error('Not implemented');
+    if ( rect1.top + rect1.width > rect2.top && rect1.left + rect1.height > rect2.left)
+		return true;
+	if ( rect2.top + rect2.width < rect1.top && rect2.left + rect2.height < rect1.left)
+		return true;
+	return false;
 }
 
 
